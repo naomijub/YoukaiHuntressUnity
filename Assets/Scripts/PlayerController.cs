e3ccdcea4b05988238f111f5d0128615e5e360b2
naomijub <jnboeira@outlook.com>
2016-09-13 02:33:14 -0300

    Redoing repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    bool facingRight = true;
+
+    public float speed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector2(h * speed, 0);
+
+        if (h > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (h < 0 && facingRight) {
+            Flip();
+        }
+	}
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
